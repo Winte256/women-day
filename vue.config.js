b99f595902e71286cb2a12e96c7f6eebe921e446
m.krutihin <m.krutihin@gmail.com>
2022-03-07 17:57:03 +0300

    add variables

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'stylus',
+      patterns: [path.resolve(__dirname, './src/assets/styles/*.styl')],
+    },
+  },
 });
