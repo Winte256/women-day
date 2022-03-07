6d94d27dfea778037bf953c71a09f852c986b436
ilya shurmanov <ilya_shurmanov@108dev.space>
2022-03-08 01:00:36 +0300

    build: your-gift

--- src/views/YourGiftView.vue
+++ src/views/YourGiftView.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="yourGift">
     <the-title class="home__title"> Ваш подарок </the-title>
-    <div class="buttonsBox">
+    <div class="imageBox">
+      <img
+        class="image"
+        src="https://oton.org/nft/drop_8_march/gift1.gif?1"
+        alt="nft flower"
+      />
+    </div>
+    <div class="yourGift__buttonsBox buttonsBox">
       <common-button :hasBackground="true">Добавить в Metamask</common-button>
-      <common-button :isBordered="true">Как привязать кошелек</common-button>
+      <common-button :isBordered="true" @click="howToAttach"
+        >Как привязать кошелек</common-button
+      >
     </div>
     <contacts-box />
   </div>
@@ -13,6 +22,7 @@
 import TheTitle from '@/components/TheTitle.vue';
 import CommonButton from '@/components/CommonButton.vue';
 import ContactsBox from '@/components/ContactsBox.vue';
+import { useRouter } from 'vue-router';
 
 export default {
   name: 'HomeView',
@@ -21,6 +31,12 @@ export default {
     CommonButton,
     ContactsBox,
   },
+  setup() {
+    const router = useRouter();
+
+    router.push('/your-gift');
+  }
+
 };
 </script>
 
@@ -33,6 +49,29 @@ export default {
   &__title {
     margin-bottom: 16px;
   }
+
+  &__buttonsBox {
+    margin-bottom: 88px;
+  }
+}
+
+.imageBox {
+  margin-bottom: 32px;
+  max-width: $width.phoneXs.buttonText;
+  height: 420px;
+  background: rgba(3, 1, 6, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.31);
+  border-radius: 8px;
+
+  +mediaPhoneS() {
+    min-width: $width.phoneS.buttonText;
+  }
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: 100%;
 }
 
 .buttonsBox {
